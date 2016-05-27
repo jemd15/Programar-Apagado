2260bf81d1b442af05c19b04b4645c30542f3ddd
Javier Muñoz <Javier Muoz>
2016-05-27 23:52:23 +0800

    modificaciones al crear el instalador de la nueva version

--- ProgramarApagado/ComandosCMD.cs
+++ ProgramarApagado/ComandosCMD.cs
@@ -41,5 +41,21 @@ namespace ProgramarApagado
 
         }
 
+        public static void ProgramarHibernar(decimal tiempo) {
+            //Indicamos el comando a ejecutar en la consola de comandos
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c shutdown /h /f /t " + tiempo);
+            //Indicamos que la salida del proceso se redireccione en un Stream
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.UseShellExecute = false;
+            //Indica que el proceso no despliegue una pantalla negra (El proceso se ejecuta en background)
+            procStartInfo.CreateNoWindow = true;
+            //Esconder la ventana
+            procStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            //Inicializa el proceso
+            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+            proc.StartInfo = procStartInfo;
+            proc.Start();
+        }
+
     }
 }
